a6b3d148cdb94943187103d9db1eec99b2bb76d7
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-10-02 17:00:33 +0200

    Listing 7.3 - Enabling test support for TestNG

--- todo/repository/src/test/java/com/eara/gia/todo/repository/impl/TodoRepositoryInMemoryImplTest.java
+++ todo/repository/src/test/java/com/eara/gia/todo/repository/impl/TodoRepositoryInMemoryImplTest.java
@@ -2,9 +2,8 @@ package com.eara.gia.todo.repository.impl;
 
 import com.eara.gia.todo.model.ToDoItem;
 import com.eara.gia.todo.repository.TodoRepository;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -12,7 +11,7 @@ import static org.junit.Assert.assertNotNull;
 public class TodoRepositoryInMemoryImplTest {
 
     private TodoRepository todoImMemoryRepositoryImpl;
-
+/**
     @Before
     public void setUp() throws Exception {
         todoImMemoryRepositoryImpl = new TodoRepositoryInMemoryImpl();
@@ -50,6 +49,50 @@ public class TodoRepositoryInMemoryImplTest {
     }
 
     @Test
+    public void delete() {
+    }*/
+
+    /**
+     * Using TestNG
+     */
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        todoImMemoryRepositoryImpl = new TodoRepositoryInMemoryImpl();
+    }
+
+    @AfterClass
+    public void tearDown() throws Exception {
+    }
+
+    @org.testng.annotations.Test
+    public void findAll() {
+    }
+
+    @org.testng.annotations.Test
+    public void findById() {
+    }
+
+    @org.testng.annotations.Test
+    public void insert() {
+        ToDoItem newToDoItem = new ToDoItem();
+
+        newToDoItem.setName("Write unit tests");
+        Long newId = todoImMemoryRepositoryImpl.insert(newToDoItem);
+
+        assertNotNull(newId );
+
+        ToDoItem persistedTodoItem = todoImMemoryRepositoryImpl.findById(newId);
+
+        assertNotNull(persistedTodoItem);
+        assertEquals(newToDoItem, persistedTodoItem);
+    }
+
+    @org.testng.annotations.Test
+    public void update() {
+    }
+
+    @org.testng.annotations.Test
     public void delete() {
     }
 }
